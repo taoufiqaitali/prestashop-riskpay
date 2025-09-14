b3fd9abf745178cea9e93da52af3a8377c52a6bb
taoufik ait ali <taitali@sqli.com>
2025-09-14 16:59:22 +0100

    validation and support for prestashop 9

--- riskpay.php
+++ riskpay.php
@@ -1,4 +1,29 @@
 <?php
+/**
+* 2007-2025 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author    PrestaShop SA <contact@prestashop.com>
+*  @copyright 2007-2025 PrestaShop SA
+*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
 if (!defined('_PS_VERSION_')) {
 	exit;
 }
@@ -9,7 +34,7 @@ class RiskPay extends PaymentModule
 	{
 		$this->name = 'riskpay';
 		$this->tab = 'payments_gateways';
-		$this->version = '1.0.0';
+		$this->version = '1.1.0';
 		$this->author = 'Taoufiq Ait Ali';
 		$this->controllers = array('payment', 'validation');
 		$this->currencies = true;
@@ -86,7 +111,8 @@ class RiskPay extends PaymentModule
 			$helper->module = $this;
 			$helper->name_controller = $this->name;
 			$helper->token = Tools::getAdminTokenLite('AdminModules');
-			$helper->currentIndex = AdminController::$currentIndex.'&configure='.$this->name;
+			$helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false) . '&configure='
+            . $this->name . '&tab_module=' . $this->tab . '&module_name=' . $this->name;
 			$helper->default_form_language = (int)Configuration::get('PS_LANG_DEFAULT');
 			$helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') ? Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') : 0;
 			$helper->title = $this->displayName;
@@ -102,18 +128,19 @@ class RiskPay extends PaymentModule
 			/* add pending order state */
 			$OrderPending              = new OrderState();
 			$OrderPending->name        = array_fill(0, 10, 'AWAITING RISKPAY PAYMENT');
-			$OrderPending->send_email  = 0;
-			$OrderPending->invoice     = 0;
+			$OrderPending->module_name = $this->name;
+			$OrderPending->send_email  = false;
+			$OrderPending->invoice     = false;
 			$OrderPending->color       = 'RoyalBlue';
 			$OrderPending->unremovable = true;
-			$OrderPending->logable     = 0;
+			$OrderPending->logable     = false;
 			$OrderPending->add();
+			Configuration::updateValue('RISKPAY_PENDING', (int)$OrderPending->id);
 		} catch (\Exception $e) {
 			// no need to do anything, the state already exists
+			Configuration::updateValue('RISKPAY_PENDING', _PS_OS_BANKWIRE_);
 		}
-        
 
-        Configuration::updateValue('RISKPAY_PENDING', $OrderPending->id);
 		return parent::install() && $this->registerHook('payment') && $this->registerHook('paymentReturn') && $this->registerHook('paymentOptions');
 	}
 
@@ -156,7 +183,7 @@ class RiskPay extends PaymentModule
 		$payment_options = [];
 
 		$option = new \PrestaShop\PrestaShop\Core\Payment\PaymentOption();
-		$option->setCallToActionText($this->l('Pay by Credit Card'));
+		$option->setCallToActionText($this->l('Pay via credit card'));
 		//$option->setLogo(Media::getMediaPath(_PS_MODULE_DIR_.$this->name.'/logo.png'));
 		$option->setAction($this->context->link->getModuleLink($this->name, 'payment', array(), true));
 		$payment_options[] = $option;
@@ -168,6 +195,7 @@ class RiskPay extends PaymentModule
 		if (!$this->active) {
 			return;
 		}
+
 		return $this->display(__FILE__, 'views/templates/hook/payment_return.tpl');
 	}
 }
